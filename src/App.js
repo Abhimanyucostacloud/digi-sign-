c06048bb5eb1155d5cf67e3e2ddf281adc3b6bba
AbhimanyuSolanki10 <abhimanyusolanki10@gmail.com>
2025-02-24 13:35:46 +0530

    dynamic upload pdf

--- src/App.js
+++ src/App.js
@@ -59,7 +59,8 @@ const App = () => {
     console.log("Password:", digitalIDPassword);
     console.log("Signature Info:", signatureInfo);
   };
-
+   
+  
   return (
     <Box sx={{ display: "flex", height: "100vh", overflow: "hidden" }}>
       {/* WebViewer on the left */}
